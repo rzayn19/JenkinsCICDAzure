17a3d32d3b224f973faee2868d655cc7ba816cf5
rzayn19 <rzayn19@yahoo.in>
2023-03-26 23:13:08 +0100

    changes

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   features {}
-  subscription_id = "5611a21a-1f28-4841-8d8f-8ef4add1359a"
-  client_id       = "e5b62a78-db10-45f5-b147-805784ba4d62"
-  client_secret   = "hTl8Q~kthMRinTifZ9eWIL0jB0tRSHWAJkxssbp0"
-  tenant_id       = "b7102f79-320b-4eb4-afe2-e14b232e19b1"
+  subscription_id = "var.subscrition_id"
+  client_id       = "var.client_id"
+  client_secret   = "var.client_secret"
+  tenant_id       = "var.tenant_id"
 }
 
 resource "azurerm_resource_group" "rg" {
