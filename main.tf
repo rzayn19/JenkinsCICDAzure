4198c80dcd80a29aa70604cbb9a598a88bac6c05
rzayn19 <rzayn19@yahoo.in>
2023-03-26 17:24:14 +0100

    Initial commit

--- main.tf
+++ main.tf
@@ -1,58 +1,120 @@
 provider "azurerm" {
   features {}
+  subscription_id = "5611a21a-1f28-4841-8d8f-8ef4add1359a"
+  client_id       = "e5b62a78-db10-45f5-b147-805784ba4d62"
+  client_secret   = "hTl8Q~kthMRinTifZ9eWIL0jB0tRSHWAJkxssbp0"
+  tenant_id       = "b7102f79-320b-4eb4-afe2-e14b232e19b1"
 }
 
-resource "azurerm_resource_group" "rd" {
-  name     = "Prodrd"
-  location = "eastus"
+resource "azurerm_resource_group" "rg" {
+  location = "UK South"
+  name     = "prodrd-rg"
 }
 
-resource "azurerm_virtual_network" "vpc" {
-  name                = "Prodrdvpc"
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "prodrd-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = "eastus"
-  resource_group_name = azurerm_resource_group.rd.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_subnet" "frontend" {
-  name                 = "frontend"
-  resource_group_name  = azurerm_resource_group.rd.name
-  virtual_network_name = azurerm_virtual_network.vpc.name
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "prodrd-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# Create a storage account for the virtual machine disks
-resource "azurerm_storage_account" "rd" {
-  name                     = "Prodrdsa"
-  resource_group_name      = azurerm_resource_group.rd.name
-  location                 = azurerm_resource_group.rd.location
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "prodrd-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rules
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "prodrd-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "web"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "prodrd-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = "prodrdsa"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
-# Create an Azure SQL Virtual Machine
-resource "azurerm_sql_virtual_machine" "rd" {
-  name                = "ProdrdSQLVM"
-  location            = azurerm_resource_group.rd.location
-  resource_group_name = azurerm_resource_group.rd.name
-  subnet_id           = azurerm_subnet.rd.id
-  sql_server_id       = azurerm_sql_server.rd.id
-  admin_username      = "rdadmin"
-  admin_password      = "Tech@123"
-
-# Specify the virtual machine configuration
-  vm_size     = "Standard_B2ms"
-  offer       = "sql2022-ws2022"
-  publisher   = "MicrosoftSQLServer"
-  sku         = "sqldev-gen2"
-  version     = "latest"
-  license_type = "PAYG"
 
 
-  storage_os_disk {
-    name              = "rd-os-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+# Create virtual machine
+resource "azurerm_windows_virtual_machine" "main" {
+  name                  = "prodrdsqlvm"
+  admin_username        = "rahul"
+  admin_password        = "Chudaman@12345"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  size                  = "Standard_B2ms"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
   }
 
-
+  source_image_reference {
+    publisher = "MicrosoftSQLServer"
+    offer     = "sql2022-ws2022"
+    sku       = "sqldev-gen2"
+    version   = "latest"
+  }
+}
