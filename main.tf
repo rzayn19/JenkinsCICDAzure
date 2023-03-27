e8377a81a40efbb07d39c972a00d3b0cbe0760b2
rzayn19 <rzayn19@yahoo.in>
2023-03-28 00:31:50 +0100

    changes

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   features {}
-  subscription_id = "${var.credentials}"
-  client_id       = "${var.credentials}"  
-  client_secret   = "${var.credentials}"
-  tenant_id       = "${var.credentials}"
+  subscription_id = "var.subscription_id"
+  client_id       = "var.client_id"  
+  client_secret   = "var.client_secret"
+  tenant_id       = "var.tenant_id"
 }
 
 resource "azurerm_resource_group" "rg" {
