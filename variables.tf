71a2805f0cc145cc5ebcbe9ff3236a7d1ce363fa
rzayn19 <rzayn19@yahoo.in>
2023-03-27 23:21:49 +0100

    changes

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "client_id" {
+  description = "My Client id"
+}
+
+variable "subscription_id" {
+  description = "My Subs id"
+}
+
+variable "client_Secret" {
+  description = "My client Secret"
+}
+
+variable "tenant_id" {
+  description = "My tenant id"
+}
