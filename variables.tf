a0fb2eea37c707d891fb2384c0c2122149c97e66
rzayn19 <rzayn19@yahoo.in>
2023-03-28 00:13:43 +0100

    changes

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "credentials" {
    type = map
    default = { 
-
   subscription_id = "5611a21a-1f28-4841-8d8f-8ef4add1359a"
   client_id       = "e5b62a78-db10-45f5-b147-805784ba4d62"
   client_secret   = "hTl8Q~kthMRinTifZ9eWIL0jB0tRSHWAJkxssbp0"
