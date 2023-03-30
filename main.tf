ceda691ee9bf970c165260c4b242df62e01de556
Rahul ☁️ <69720559+rzayn19@users.noreply.github.com>
2023-03-30 13:38:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source ="hashicorp/azurerm"
+      version ="=2.71.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "var.subscription_id"
