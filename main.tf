8d50b3491ea3812d3a5c804fd7c4146cc8af995e
Rahul ☁️ <69720559+rzayn19@users.noreply.github.com>
2023-04-06 16:50:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
   client_id       = "var.client_id"  
   client_secret   = "var.client_secret"
   tenant_id       = "var.tenant_id"
+  redirect_url    = "var.redirect_uri"
 }
 
 resource "azurerm_resource_group" "rg" {
